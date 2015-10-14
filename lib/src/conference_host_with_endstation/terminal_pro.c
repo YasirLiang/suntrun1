fbcdb862ab45c2f626c06e952a0aaef175e48bad
lyf <lyf@ubun.(none)>
2015-10-14 09:26:36 +0800

    change a little

--- lib/src/conference_host_with_endstation/terminal_pro.c
+++ lib/src/conference_host_with_endstation/terminal_pro.c
@@ -38,7 +38,7 @@ void init_terminal_proccess_system( void )
 
 	// init terminal system double list
 	init_terminal_dblist( &dev_terminal_list_guard );
-	assert( dev_terminal_list_guard != NULL);
+	assert( dev_terminal_list_guard != NULL );
 }
 
 // send terminal conference deal message in 1722 frame payload by pipe
@@ -52,8 +52,8 @@ uint16_t ternminal_send( void *buf, uint16_t length, uint64_t uint64_target_id )
 	int send_len = 0;
 	int cnf_data_len = 0;
 
-	memcpy(send_frame.src_address.value, net.m_my_mac, 6);
-	convert_uint64_to_eui64( target_id.value, uint64_target_id);
+	memcpy( send_frame.src_address.value, net.m_my_mac, 6 );
+	convert_uint64_to_eui64( target_id.value, uint64_target_id );
 	cnf_data_len = conference_host_to_end_form_msg( &send_frame, &fill_send_buf, data_buf->cchdr.command_control, data_buf->data_len, data_buf->cchdr.address, data_buf->data );
 	send_len = conference_1722_control_form_info( &send_frame, &aemdu, jdksavdecc_multicast_adp_acmp, target_id, cnf_data_len );
 	if( send_len < 0 )
@@ -72,7 +72,7 @@ uint16_t ternminal_send( void *buf, uint16_t length, uint64_t uint64_target_id )
 void terminal_recv_message_pro( struct terminal_deal_frame *conference_frame )
 {
 	assert( NULL != conference_frame );
-	DEBUG_RECV( conference_frame->payload , conference_frame->payload_len, "Conference Data Recv" );
+	DEBUG_RECV( conference_frame->payload, conference_frame->payload_len, "Conference Data Recv" );
 }
 
 void host_reply_terminal()
