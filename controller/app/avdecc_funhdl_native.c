8ca637338012447e28d08fcf99219fc3bbf6809b
YasirLiang <m15915849649@163.com>
2016-09-21 17:44:24 +0800

    (arm compiler)change avdecc_funhdl_native.c/.h according to MISRA coding standard

--- controller/app/avdecc_funhdl_native.c
+++ controller/app/avdecc_funhdl_native.c
@@ -1,188 +1,221 @@
-#include "avdecc_funhdl_native.h"
+/*
+* @file
+* @brief avdecc funhdl native
+* @ingroup avdecc funhdl native
+* @cond
+******************************************************************************
+* Last updated for version 1.0.0
+* Last updated on  2016-09-21
+*
+*                    Moltanisk Liang
+*                    ---------------------------
+*                    avb auto control system
+*
+* Copyright (C) Moltanisk Liang, GuangZhou Suntron. All rights reserved.
+******************************************************************************
+* @endcond
+*/
+/*Including files-----------------------------------------------------------*/
+#include "avdecc_main.h"
+#include "system_packet_tx.h"
+#include "host_controller_debug.h"
 #include "message_queue.h"
 #include "send_work_queue.h"
 #include "system_packet_tx.h"
 #include "send_common.h"
 #include "control_matrix_common.h"
 #include "time_handle.h"
-#include "avdecc_manage.h"// 发现终端，读描述符，移除终端
-#include "send_common.h" // 包含SEND_DOUBLE_QUEUE_EABLE
+#include "avdecc_manage.h"/* discover and remove terminal */
+#include "send_common.h" /* Including SEND_DOUBLE_QUEUE_EABLE*/
 #include "camera_pro.h"
 #include "system_1722_recv_handle.h"
 #include "terminal_pro.h"
 #include "muticast_connect_manager.h"
 #include "global.h"
+#include <pthread.h>
+#include "func_proccess.h"
+#include "avdecc_funhdl_native.h"
 
-int thread_func_fn( void * pgm )
-{
-	fcwqueue*  p_func_wq = &fcwork_queue;
-	assert( pgm && p_func_wq );
-	
-	proccess_func_items *p_func_items = (proccess_func_items *)pgm;	
-	fcqueue_wnode *p_msg_wnode = NULL;
-	
-	/* 处理命令函数 */
-	while( 1 )
-	{
-		pthread_mutex_lock( &p_func_wq->control.mutex );
-		
-		while( p_func_wq->control.active && (p_func_wq->work.head == NULL) )
-		{
-			pthread_cond_wait( &p_func_wq->control.cond, &p_func_wq->control.mutex );
-			DEBUG_INFO( "active = %d", p_func_wq->control.active );
-		}
-
-		if( !p_func_wq->control.active )
-		{
-			pthread_mutex_unlock( &p_func_wq->control.mutex );
-			break;
-		}
-
-		p_msg_wnode = func_command_work_queue_messag_get( p_func_wq );
-		if( p_func_wq->work.head == NULL )
-		{
-			if( p_func_wq->work.trail != NULL ) 
-			{// while queue isempty,make sure the queue trail not to be used again!!!!
-				p_func_wq->work.trail = NULL;
-			}
-		}
-		if( NULL == p_msg_wnode )
-		{
-			DEBUG_INFO( "func work queue no node!" );
-			pthread_mutex_unlock( &p_func_wq->control.mutex );
-			continue;
-		}
-		
-		// save func command queue message
-		uint16_t func_index = p_msg_wnode->job_data.func_msg_head.func_index;
-		uint16_t func_cmd = p_msg_wnode->job_data.func_msg_head.func_cmd;
-		uint32_t data_len = p_msg_wnode->job_data.meet_msg.data_len;
-		uint8_t func_data[SUB_DATA_TYPE_SIZE] = {0};
-		if( data_len > SUB_DATA_TYPE_SIZE )
-		{
-			if( NULL != p_msg_wnode )
-			{
-				free( p_msg_wnode );
-				p_msg_wnode = NULL;
-			}
-		
-			pthread_mutex_unlock( &p_func_wq->control.mutex ); // unlock mutex
-			continue;
-		}
-		
-		memcpy( func_data, p_msg_wnode->job_data.meet_msg.data_buf, data_len );
-		if( NULL != p_msg_wnode )
-		{
-			free( p_msg_wnode );
-			p_msg_wnode = NULL;
-		}
-		
-		pthread_mutex_unlock( &p_func_wq->control.mutex ); // unlock mutex
-
-		DEBUG_INFO( "Run func comand index = %d", func_index );
-		p_func_items[func_index].cmd_proccess( func_cmd, func_data, data_len );// run command
-	}
-	
-	return 0;
-}
-
-int pthread_handle_cmd_func( pthread_t *pid, const proccess_func_items *p_items )
-{
-	assert( pid );
-	int rc = 0;
-
-	rc = pthread_create( pid, NULL, (void*)&thread_func_fn, (void*)p_items );
-	if( rc )
-	{
-		DEBUG_INFO(" pthread_handle_cmd_func ERROR; return code from pthread_create() is %d\n", rc);
-		assert( rc == 0 );
-	}
-
-	return 0;
-}
-
+/*Macro INPUT_MSG_LEN for control surface message len-----------------------*/
 #define INPUT_MSG_LEN	6
-extern void input_recv_pro(unsigned char *p_buf, unsigned recv_len);
-extern unsigned char gcontrol_sur_recv_buf[INPUT_MSG_LEN]; 
-extern volatile unsigned char gcontrol_sur_msg_len; 
+/*Macro SYS_BUF_RECV_COUNT buffer counts------------------------------------*/
 #define SYS_BUF_RECV_COUNT 2
-int pthread_recv_data_fn( void *pgm )
-{
-	static int static_buf_num = 0;
+/*Extern function delaration------------------------------------------------*/
+extern void input_recv_pro(unsigned char *p_buf, unsigned recv_len);
+/*Extern function delaration------------------------------------------------*/
+extern unsigned char gcontrol_sur_recv_buf[INPUT_MSG_LEN]; 
+/*Extern Global vaviable delaration-----------------------------------------*/
+extern volatile unsigned char gcontrol_sur_msg_len;
 
-	while( 1 )
-	{
-		unsigned long us_per_ms = 1000;
-        	unsigned long interval_ms = 0/*TIME_PERIOD_1_MILLISECONDS*/;
-		struct timeval tempval;
+/*$ command function thread proccess........................................*/
+int thread_func_fn(void * pgm) {
+    /* pointer to function working queue */
+    fcwqueue* p_func_wq = &fcwork_queue;
+    assert( pgm != NULL && p_func_wq != NULL);
+    /* command funtion table pointer */
+    proccess_func_items *p_func_items = (proccess_func_items *)pgm;
+    /* queue node pointer of  command queue */
+    fcqueue_wnode *p_msg_wnode = NULL;
 
-		tempval.tv_sec = interval_ms/1000;  
-	        tempval.tv_usec = (interval_ms%1000)*us_per_ms;
-	        select( 0, NULL, NULL, NULL, &tempval );
+    /* The thread will be waked up by other threads which  save queue
+    node to command working queue.When the thread wake up by
+    recieving pthread cond signal, this thread will run the command
+    function according to the command function index from queue node
+    */
+    while (1) {
+        /* lock queue before operating first */
+        pthread_mutex_lock(&p_func_wq->control.mutex);
+        while ((p_func_wq->control.active)
+                    && (p_func_wq->work.head == NULL))
+        {
+            /* thread sleep until recieving pthread cond signal */
+            pthread_cond_wait(&p_func_wq->control.cond,
+                                                   &p_func_wq->control.mutex);
+        }
 
-		if( static_buf_num >= SYS_BUF_RECV_COUNT )
-			static_buf_num = 0;
-		switch( (static_buf_num++) % SYS_BUF_RECV_COUNT )
-		{
-			case 0:// buffer 1
-				if (gregister_tmnl_pro.rgs_state == RGST_IDLE)
-				{
-					pthread_mutex_lock(&ginflight_pro.mutex);
-					upper_computer_recv_message_get_pro();
-					pthread_mutex_unlock(&ginflight_pro.mutex);
-				}
-			break;
-			case 1:// buffer 2
-				control_matrix_common_recv_message_pro();
-			break;
-			default:
-				break;
-		}
-        
-		system_register_terminal_pro();
-		if (gregister_tmnl_pro.rgs_state == RGST_IDLE)
-		{
-			terminal_sign_in_pro();// 注册终端,在获取系统信息成功后，每隔一定的时间注册一个
-			terminal_vote_proccess();// 终端投票处理
-			terminal_query_sign_vote_pro();// 查询终端的签到与投票结果
+        if (!p_func_wq->control.active) {
+            /* unlock queue because the queue not active */
+            pthread_mutex_unlock( &p_func_wq->control.mutex);
+            break;
+        }
+        /* get queue node from queue */
+        p_msg_wnode = func_command_work_queue_messag_get(p_func_wq);
+        if (p_func_wq->work.head == NULL) {
+            if (p_func_wq->work.trail != NULL) {
+                /*while queue isempty,
+                make sure the queue trail not to be used again*/
+                p_func_wq->work.trail = NULL;
+            }
+        }
+        if (NULL == p_msg_wnode) {
+            /* before continue must unlock the mutex */
+            pthread_mutex_unlock(&p_func_wq->control.mutex);
+            continue;/*get wrong node from the queue*/
+        }
 
-#ifdef __ARM_BACK_TRACE__
-                        /*
-			  *串口接收数据处理与菜单显示处理
-			  */
-			if( gcontrol_sur_msg_len > 0 )
-			{
-				input_recv_pro( gcontrol_sur_recv_buf, gcontrol_sur_msg_len );
-				gcontrol_sur_msg_len = 0;
-			}
-#endif
+        /* get func command queue message from work node */
+        uint16_t func_index = p_msg_wnode->job_data.func_msg_head.func_index;
+        uint16_t func_cmd = p_msg_wnode->job_data.func_msg_head.func_cmd;
+        uint32_t data_len = p_msg_wnode->job_data.meet_msg.data_len;
+        uint8_t func_data[SUB_DATA_TYPE_SIZE] = {0};/* reset buffer to zero */
+        if (data_len > SUB_DATA_TYPE_SIZE) {
+            if (NULL != p_msg_wnode) {
+                free(p_msg_wnode);/*free space*/
+                p_msg_wnode = NULL;/*make NULL*/
+            }
+            /* before continue must unlock the mutex */
+            pthread_mutex_unlock( &p_func_wq->control.mutex);
+            continue;/* get data length */
+        }
+        /* copy function data */
+        memcpy(func_data, p_msg_wnode->job_data.meet_msg.data_buf, data_len);
+        if (NULL != p_msg_wnode) {
+            free( p_msg_wnode );
+            p_msg_wnode = NULL;
+        }
+        /* before run command  must unlock the mutex for other thread can save
+            comand function node to this queue in the meantime */
+        pthread_mutex_unlock( &p_func_wq->control.mutex );
+        /* run function command */
+        p_func_items[func_index].cmd_proccess(func_cmd, func_data, data_len);
+    }
 
-			muticast_connect_manger_timeout_event_image();
-		}
-		
-		avdecc_manage_discover_proccess();// 系统定时发现终端
-		send_common_check_squeue();// 检查发送队列发送数据
-		camera_pro();// 摄像头处理流程
-		terminal_over_time_speak_pro();//
-		terminal_after_time_mic_state_pro();
-                Terminal_micCallbackPro();
-	}
-	
-	return 0;
+    return 0;
 }
-
-int pthread_proccess_recv_data_create( pthread_t *pid, void * pgm )
+/*$ for create the thread of command function...............................*/
+int pthread_handle_cmd_func(pthread_t *pid,
+                                           const proccess_func_items *p_items)
 {
-	assert( pid );
-	int rc = 0;
+    int rc = 0;   /* pthread_create function return value */
+    assert(NULL != pid); /* make sure pid pointer is available */
+    rc = pthread_create(pid, NULL, (void*)&thread_func_fn, (void*)p_items);
+    if (rc) {
+        DEBUG_INFO("thread Create failed: rc = %d", rc);
+        assert(rc == 0);
+    }
+    return 0;
+}
+/*$ thread funtion..........................................................*/
+int pthread_recv_data_fn(void *pgm) {
+    static int static_buf_num = 0;/*for change buffer*/
+    while(1) {
+        unsigned long us_per_ms = 1000;
+        unsigned long interval_ms = 0;
+        struct timeval tempval;
 
-	rc = pthread_create( pid, NULL, (void*)&pthread_recv_data_fn, NULL );
-	if( rc )
-	{
-		DEBUG_INFO(" pthread_proccess_recv_data_create ERROR; return code from pthread_create() is %d\n", rc);
-		assert( rc == 0 );
-	}
+        tempval.tv_sec = interval_ms/1000;  
+        tempval.tv_usec = (interval_ms%1000)*us_per_ms;
+        select( 0, NULL, NULL, NULL, &tempval );/*wait for a time*/
 
-	return 0;
+        if (static_buf_num >= SYS_BUF_RECV_COUNT) {
+            static_buf_num = 0;
+        }
+
+        switch ((static_buf_num++) % SYS_BUF_RECV_COUNT) {
+            case 0: {/* switch to buffer 1 for proccessing recieve data */
+                if (gregister_tmnl_pro.rgs_state == RGST_IDLE) {
+                    pthread_mutex_lock(&ginflight_pro.mutex);
+                    /* proccessing the data from upper computer */
+                    upper_computer_recv_message_get_pro();
+                    pthread_mutex_unlock(&ginflight_pro.mutex);
+                }
+                break;
+            }
+            case 1: {/* switch to buffer 2 for proccessing recieve data */
+                control_matrix_common_recv_message_pro();
+                break;
+            }
+            default: {/* will never come this case */
+                break;
+            }
+        }
+
+        /* proccess termianl point register */
+        system_register_terminal_pro();
+        if (gregister_tmnl_pro.rgs_state == RGST_IDLE) {
+            /* proccess terminal of not signing success */
+            terminal_sign_in_pro();
+            /* proccess voting */
+            terminal_vote_proccess();
+            /* query the result of sign and vote */
+            terminal_query_sign_vote_pro();
+            /* enable arm version when __ARM_BACK_TRACE__ is defined */
+#ifdef __ARM_BACK_TRACE__
+            /*proccessing the menu display according to the data recieving from*/
+            if (gcontrol_sur_msg_len > 0) {
+                input_recv_pro(gcontrol_sur_recv_buf, gcontrol_sur_msg_len);
+                gcontrol_sur_msg_len = 0;
+            }
+#endif
+            /*muticast connect table manager*/
+            muticast_connect_manger_timeout_event_image();
+        }
+        
+        /* avdecc discover terminal managing */
+        avdecc_manage_discover_proccess();
+        /* check send queue is empty? */
+        send_common_check_squeue();
+        /* camera proccessing */
+        camera_pro();
+        /* speaking timeout proccessing */
+        terminal_over_time_speak_pro();
+        /* microphone state set later*/
+        terminal_after_time_mic_state_pro();
+        /*mic state set callback proccessing */
+        Terminal_micCallbackPro();
+    }	
+    return 0;
+}
+/*$ create the thread for proccessing recieve data..........................*/
+int pthread_proccess_recv_data_create(pthread_t *pid, void * pgm) {
+    int rc = 0; /* pthread_create function return value */
+    assert(NULL != pid);/* make sure pid pointer is available */
+    rc = pthread_create(pid, NULL, (void*)&pthread_recv_data_fn, NULL);
+    if(rc) {
+        DEBUG_INFO("recv thread Create failed: rc = %d", rc);
+        assert(rc == 0);
+    }
+
+    return 0;
 }
 
--- controller/app/include/avdecc_funhdl_native.h
+++ controller/app/include/avdecc_funhdl_native.h
@@ -1,17 +1,38 @@
+/*
+* @file
+* @brief avdecc funhdl native
+* @ingroup avdecc funhdl native
+* @cond
+******************************************************************************
+* Last updated for version 1.0.0
+* Last updated on  2016-09-21
+*
+*                    Moltanisk Liang
+*                    ---------------------------
+*                    avb auto control system
+*
+* Copyright (C) Moltanisk Liang, GuangZhou Suntron. All rights reserved.
+******************************************************************************
+* @endcond
+*/
 #ifndef __AVDECC_FUNHDL_NATIVE_H__
 #define __AVDECC_FUNHDL_NATIVE_H__
 
-#include <pthread.h>
-#include "avdecc_main.h"
-#include "system_packet_tx.h"
-#include "host_controller_debug.h"
+/*! handle command funtion thread create*/
+extern int pthread_handle_cmd_func(pthread_t *pid,
+                                          const proccess_func_items *p_items);
+/*! proccess recieve data and a little funtion proccess thread create */
+extern int pthread_proccess_recv_data_create(pthread_t *pid, void * pgm);
 
-int thread_pipe_fn( void *pgm );
-int pthread_handle_pipe( pthread_t *p_trd, struct fds *kfds );
-int pthread_handle_cmd_func( pthread_t *pid, const proccess_func_items *p_items );
-int thread_func_fn( void * pgm );
-int pthread_recv_data_fn( void *pgm );
-int pthread_proccess_recv_data_create( pthread_t *pid, void * pgm );
+#endif /*__AVDECC_FUNHDL_NATIVE_H__*/
 
-#endif
+/*****************************************************************************
+*NOTE01:
+*pthread.h must included before including 'avdecc_funhdl_native.h', 
+*because of pthread_t not declaring in 'avdecc_funhdl_native.h'.
+*
+*NOTE02:
+*func_proccess.h must included before including 'avdecc_funhdl_native.h', 
+*because of pthread_t not declaring in 'avdecc_funhdl_native.h'.
+*/
 
